d32744baafd8b590e56d6bd14442177012041b66
Kim 金可明 <terra@proglab.nl>
2025-02-16 13:29:05 +0100

    Update mario.c

--- mario.c
+++ mario.c
@@ -1,8 +1,14 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<string.h>
 
-int main(void)
+int main()
 {
-    printf("Hello!\n");
+    char source[] = "GeeksForGeeks";
 
+    // A copy of source is created dynamically
+    // and pointer to copy is returned.
+    char* target = strdup(source); 
+
+    printf("%s", target);
     return 0;
 }
