354cd03bbafa9ad24c8c7f8395be5dc562839dc9
Kim 金可明 <terra@proglab.nl>
2025-02-16 13:50:19 +0100

    Update mario.c

--- mario.c
+++ mario.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    argc = 3;
-    argv = test_argv;
-
     printf("argc: %d\n", argc);
     for (int i = 0; i < argc; i++) {
         printf("argv[%d]: %s\n", i, argv[i]);
