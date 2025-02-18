9522f0edc1ef96524c3513f27d63ab6fe922e190
Kim 金可明 <terra@proglab.nl>
2025-02-18 17:40:49 +0100

    Update args.c

--- args.c
+++ args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    printf("argc: %d\n", argc);
+    printf("argc:  %d\n", argc);
     for (int i = 0; i < argc; i++) {
         printf("argv[%d]: %s\n", i, argv[i]);
     }
