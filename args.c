7dd1dfa353a89d003c9a2edc7ce6bd5f4af26abe
Kim 金可明 <terra@proglab.nl>
2025-03-09 15:07:03 +0100

    Update args.c

--- args.c
+++ args.c
@@ -4,7 +4,7 @@ int main(int argc, char *argv[]) {
     printf("argc: %d\n", argc);
     
     
-    for (int i = 0; i < argc; i++) {
+    // for (int i = 0; i < argc; i++) {
         printf("argv[%d]: %s\n", i, argv[i]);
     }
     return 0;
