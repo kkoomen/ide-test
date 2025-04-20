ee1c168ec0e193d540c270f4347b841ca9d18616
UvA Programming Lab <terra@proglab.nl>
2025-04-20 14:29:46 +0200

    Update war.py

--- war.py
+++ war.py
@@ -1,8 +1,7 @@
 def bad_indent():
   x= 1 # E101: indentation is not a multiple of four
   if x>0:
-      print("This line is fine")   print("But this isn't")
-     print("This line is over 100 characters long, and it just keeps going on and on and on and on and on and on and on and doesn't stop.")  # E501
+    print("This line is over 100 characters long, and it just keeps going on and on and on and on and on and on and on and doesn't stop.")  # E501
   print("This line has trailing whitespace")     # W291
 
 def another_func(): print("Multiple statements on one line"); print("This line is okay but the previous wasn't.") # E502
