9c05b021c91ad56b0999a9741041ff6cff4dde17
UvA Programming Lab <terra@proglab.nl>
2025-04-20 13:56:39 +0200

    Update args.c

--- args.c
+++ args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     for (int i = 0; i < argc; i++) {
         printf("[%d] %s", i, argv[i]);
