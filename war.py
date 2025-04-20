edca8f2bc09ea8dd09034093461574889266c06a
UvA Programming Lab <terra@proglab.nl>
2025-04-20 14:29:17 +0200

    Update war.py

--- war.py
+++ war.py
@@ -1,7 +1,7 @@
 def bad_indent():
   x= 1 # E101: indentation is not a multiple of four
   if x>0:
-      print("This line is fine")   print("But this isn't") # E112: expected an indented block, E502: the backslash is unnecessary
+      print("This line is fine")   print("But this isn't")
      print("This line is over 100 characters long, and it just keeps going on and on and on and on and on and on and on and doesn't stop.")  # E501
   print("This line has trailing whitespace")     # W291
 
