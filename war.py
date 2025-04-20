cfd5d70d5453063b33b24847fe757944808e5446
UvA Programming Lab <terra@proglab.nl>
2025-04-20 14:32:36 +0200

    Update war.py

--- war.py
+++ war.py
@@ -1,4 +1,4 @@
-def bad_indent():
+def long_lines():
     x= 1 # E101: indentation is not a multiple of four
     if x>0:
         print("This line is over 100 characters long, and it just keeps going on and on and on and on and on and on and on and doesn't stop.")
