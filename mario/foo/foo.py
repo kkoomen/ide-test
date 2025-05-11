c85fd669323668ed28d4703ba6e0c33394a26678
UvA Programming Lab <terra@proglab.nl>
2025-05-11 16:45:39 +0200

    Update mario/foo/foo.py

--- mario/foo/foo.py
+++ mario/foo/foo.py
@@ -1,16 +1,16 @@
 import matplotlib.pyplot as plt
 
-# Sample data
-x = [1, 2, 3, 4, 5]
-y = [1, 4, 9, 16, 25]
+# # Sample data
+# x = [1, 2, 3, 4, 5]
+# y = [1, 4, 9, 16, 25]
 
-# Create the plot
-plt.plot(x, y)
+# # Create the plot
+# plt.plot(x, y)
 
-# Add labels and title
-plt.xlabel('X-axis')
-plt.ylabel('Y-axis')
-plt.title('Simple Plot')
+# # Add labels and title
+# plt.xlabel('X-axis')
+# plt.ylabel('Y-axis')
+# plt.title('Simple Plot')
 
-# Display the plot
-plt.savefig('myfile.png')
+# # Display the plot
+# plt.savefig('myfile.png')
