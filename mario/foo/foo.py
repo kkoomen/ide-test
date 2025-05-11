d54138e3d1170cc42de1af412923dd7844842d22
UvA Programming Lab <terra@proglab.nl>
2025-05-11 15:47:05 +0200

    Update mario/foo/foo.py

--- mario/foo/foo.py
+++ mario/foo/foo.py
@@ -3,3 +3,9 @@ with open("test2.txt", "w") as f:
     f.write("This is a test file.\n")
     f.write("It contains multiple lines of text.\n")
     f.write("This is the last line.")
+    
+with open("test2.txt", "w") as f:
+    f.write("Hello, World!\n")
+    f.write("This is a test file.\n")
+    f.write("It contains multiple lines of text.\n")
+    f.write("This is the last line.")
