e2bb43eef6bb7f1a25e43cd8d7bd98fd3502ed58
UvA Programming Lab <terra@proglab.nl>
2025-05-11 09:56:30 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,1 @@
-import pycodestyle
-pycodestyle.StyleGuide(
-    select=[
-        'E101',
-        'E112',
-        'E113',
-        'E115',
-        'E116',
-        'E117',
-        'E501',
-        'E502',
-        'W505',
-        'W291'
-    ],
-    max_line_length=99,
-    max_doc_length=79
-).check_files(['war.py'])
+print('hello from test.py')
