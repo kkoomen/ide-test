eef0ccee9ceeb71d9e20aa5d6040ceb495354c57
UvA Programming Lab <terra@proglab.nl>
2025-05-11 15:47:12 +0200

    Update mario/foo/foo.py

--- mario/foo/foo.py
+++ mario/foo/foo.py
@@ -1,10 +1,10 @@
-with open("test2.txt", "w") as f:
+with open("test3.txt", "w") as f:
     f.write("Hello, World!\n")
     f.write("This is a test file.\n")
     f.write("It contains multiple lines of text.\n")
     f.write("This is the last line.")
     
-with open("test2.txt", "w") as f:
+with open("test4.txt", "w") as f:
     f.write("Hello, World!\n")
     f.write("This is a test file.\n")
     f.write("It contains multiple lines of text.\n")
