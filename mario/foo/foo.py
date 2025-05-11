1f3555772f3a259d370d15bca8ec2e85d23a95aa
UvA Programming Lab <terra@proglab.nl>
2025-05-11 16:30:50 +0200

    Update mario/foo/foo.py

--- mario/foo/foo.py
+++ mario/foo/foo.py
@@ -1,2 +1,19 @@
 
 matplotlib.use("module://matplotlib_pyodide.html5_canvas_backend")
+
+import matplotlib.pyplot as plt
+
+# Sample data
+x = [1, 2, 3, 4, 5]
+y = [1, 4, 9, 16, 25]
+
+# Create the plot
+plt.plot(x, y)
+
+# Add labels and title
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Simple Plot')
+
+# Display the plot
+plt.show()
