fb539334262675da6131bf5bccc5b32d585a4809
UvA Programming Lab <terra@proglab.nl>
2025-05-11 16:30:41 +0200

    Update mario/foo/foo.py

--- mario/foo/foo.py
+++ mario/foo/foo.py
@@ -1,11 +1,2 @@
-with open("test3.txt", "w") as f:
-    f.write("Hello, World!\n")
-    f.write("This is a test file.\n")
-    f.write("It contains multiple lines of text.\n")
-    f.write("This is the last line.")
-    
-with open("test4.txt", "w") as f:
-    f.write("Hello, World!\n")
-    f.write("This is a test file.\n")
-    f.write("It contains multiple lines of text.\n")
-    f.write("This is the last line.")
+
+matplotlib.use("module://matplotlib_pyodide.html5_canvas_backend")
