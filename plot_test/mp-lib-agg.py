ebc666bc3fdca4c4cd0ec02848d3938ce2dcba45
UvA Programming Lab <terra@proglab.nl>
2025-05-15 21:08:45 +0200

    Update plot_test/mp-lib-agg.py

--- plot_test/mp-lib-agg.py
+++ plot_test/mp-lib-agg.py
@@ -1,10 +1,8 @@
 import matplotlib.pyplot as plt
 import io
 
-# Create the plot
 fig, ax = plt.subplots()
 ax.plot([1, 2, 3], [1, 4, 9])
 ax.set_title('Sample Plot')
 
-# Save the plot to a BytesIO object
 fig.savefig("test.png", format='png')
