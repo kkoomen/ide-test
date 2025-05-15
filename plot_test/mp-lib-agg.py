7534c45e1dcb6f5df336f3adb078212e224d6ccf
UvA Programming Lab <terra@proglab.nl>
2025-05-15 21:18:00 +0200

    Update plot_test/mp-lib-agg.py

--- plot_test/mp-lib-agg.py
+++ plot_test/mp-lib-agg.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import io
 
 fig, ax = plt.subplots()
-ax.plot([1, 2, 3], [1, 4, -2])
+ax.plot([1, 2, 3], [1, 4, 9])
 ax.set_title('Sample Plot')
 
 fig.savefig("test.png", format='png')
