9e12f81a2cda256b4e8f09327fb59f763b8ed96b
UvA Programming Lab <terra@proglab.nl>
2025-05-26 10:30:20 +0200

    Update mp-lib.py

--- mp-lib.py
+++ mp-lib.py
@@ -4,7 +4,7 @@ matplotlib.use("module://matplotlib_pyodide.wasm_backend")
 import matplotlib.pyplot as plt
 
 x = [1, 2, 3, 4, 5]
-y = [i ** 2 for i in x]
+y = [i ** 2 for i in x].  
 
 plt.figure(figsize=(4, 3))
 plt.plot(x, y, marker='o')
