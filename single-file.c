5fc74c6fe2d0f3110c62a8a2c0cabe5fb8d346a8
UvA Programming Lab <terra@proglab.nl>
2025-06-15 15:17:28 +0200

    Update single-file.c

---
+++ single-file.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// Declare the helper function
+void my_helper_func(void);
+
+int main(void) {
+    printf("Calling helper function...\n");
+    my_helper_func();
+    return 0;
+}
