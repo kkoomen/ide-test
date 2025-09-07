61d79e871df3c390a0453901f550bb4b40f1bf2f
UvA Programming Lab <terra@proglab.nl>
2025-09-07 09:21:18 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
 print('hello from test.py')
+print('hello from test.py')
+print('hello from test.py')
+print('hello from test.py')
+print('hello from test.py')
+print('hello from test.py')
+print('hello from test.py')
